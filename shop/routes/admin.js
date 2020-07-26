8f98bbe4b92560ff29af0ffb0d541b3f6ca795df
Telerik Arsov <telerik007@gmail.com>
2020-07-26 17:55:23 +0300

    Admin validations

--- shop/routes/admin.js
+++ shop/routes/admin.js
@@ -50,97 +50,101 @@ var tableActions = {
     },
     delete: 
     {
-        tags: tagCategoryController.deleteTag,
-        categories: tagCategoryController.deleteCategory,
-        products: productController.deleteProduct
+        tags: { func: tagCategoryController.deleteTag, validations: [
+            check('id').notEmpty().withMessage('Id is required')
+        ]},
+        categories: { func: tagCategoryController.deleteCategory, validations: [
+            check('id').notEmpty().withMessage('Id is required')
+        ]},
+        products: { func: productController.deleteProduct, validations: [
+            check('id').notEmpty().withMessage('Id is required')
+        ]}
     },
     edit:
     {
-        tags: tagCategoryController.editTags,
-        categories: tagCategoryController.editCategory,
-        products: productController.editProduct
+        tags: { func: tagCategoryController.editTags, validations: [
+            check('name').notEmpty().withMessage('Username is required'),
+            check('color').notEmpty().withMessage('Color is required'),
+            check('id').notEmpty().withMessage('Id is required')
+        ]},
+        categories: { func: tagCategoryController.editCategory, validations: [
+            check('name').notEmpty().withMessage('Username is required'),
+            check('color').notEmpty().withMessage('Color is required'),
+            check('id').notEmpty().withMessage('Id is required')
+        ]},
+        products: { func: productController.editProduct, validations:[
+            check('name').notEmpty().withMessage('Username is required'),
+            check('manifacturer').notEmpty().withMessage('Manufacturer is required'),
+            check('description').notEmpty().withMessage('Description is required'),
+            check('cost').notEmpty().withMessage('Cost is required'),
+            check('category').notEmpty().withMessage('Category is required'),
+            check('visible').notEmpty().withMessage('Visibillity is required')
+        ]}
+    },
+    create:
+    {
+        tag: { func: tagCategoryController.createTag, validations: [
+            check('name').notEmpty().withMessage('Username is required'),
+            check('color').notEmpty().withMessage('Color is required')
+        ]},
+        category: { func: tagCategoryController.createCategory, validations: [
+            check('name').notEmpty().withMessage('Username is required'),
+            check('color').notEmpty().withMessage('Color is required')
+        ]},
+        product: { func: productController.createProduct, validations:[
+            check('name').notEmpty().withMessage('Username is required'),
+            check('manifacturer').notEmpty().withMessage('Manufacturer is required'),
+            check('description').notEmpty().withMessage('Description is required'),
+            check('cost').notEmpty().withMessage('Cost is required'),
+            check('category').notEmpty().withMessage('Category is required'),
+            check('visible').notEmpty().withMessage('Visibillity is required')
+        ]},
+        worker: { func: workerController.createWorker, validations:[
+            check('username').notEmpty().withMessage('Username is required'),
+            check('email').notEmpty().withMessage('Email is required'),
+            check('email').isEmail().withMessage('Email is not valid'),
+            check('pass').notEmpty().withMessage('Password is required').custom((value, {req}) => {
+                if(value !== req.body.repass){
+                    throw new Error("Passwords do not match");
+                }
+                return true;
+            })
+        ]}
     }
 };
 
-router.get('/getAll/:table', function(req, res){
+router.get('/getAll/:table', async function(req, res){
     if(tableActions.get[req.params['table']]){
         tableActions.get[req.params['table']](req, res)
     }
 });
-router.post('/delete/:table', function(req, res){
-    if(tableActions.delete[req.params['table']]){
-        tableActions.delete[req.params['table']](req, res)
-    }
+router.post('/delete/:table', async function(req, res){
+    await validateReq('table', 'delete', req, res);
 });
 
-router.post('/edit/:table', function(req, res){
-    if(tableActions.edit[req.params['table']]){
-        tableActions.edit[req.params['table']](req, res)
-    }
+router.post('/edit/:table', async function(req, res){
+    await validateReq('table', 'edit', req, res);
 });
 
-router.post('/create/tag', [
-    check('name').notEmpty().withMessage('Username is required'),
-    check('color').notEmpty().withMessage('Color is required')
-],  function(req, res){
-        var errors = validationResult(req).array()
-        console.log(errors)
-        if (errors.length) {
-            res.json({errors: errors})
-        }else {
-            tagCategoryController.createTag(req, res)
-        }
+router.post('/create/:type' , async function(req, res){
+    await validateReq('type', 'create', req, res);
 })
 
-router.post('/create/category', [
-    check('name').notEmpty().withMessage('Username is required'),
-    check('color').notEmpty().withMessage('Color is required')
-],  function(req, res){
+async function validateReq(paramName, type, req, res){
+    if(tableActions[type][req.params[paramName]]){
+        await Promise.all(tableActions[type][req.params[paramName]]
+            .validations.map(async (element) => {
+            await element.run(req)
+        }));
         var errors = validationResult(req).array()
         console.log(errors)
         if (errors.length) {
             res.json({errors: errors})
         }else {
-            tagCategoryController.createCategory(req, res)
+            tableActions[type][req.params[paramName]].func(req, res)
         }
-})
-
-router.post('/create/product', [
-    check('name').notEmpty().withMessage('Username is required'),
-    check('manifacturer').notEmpty().withMessage('Manufacturer is required'),
-    check('description').notEmpty().withMessage('Description is required'),
-    check('cost').notEmpty().withMessage('Cost is required'),
-    check('category').notEmpty().withMessage('Category is required'),
-    check('visible').notEmpty().withMessage('Visibillity is required')
-],  function(req, res){
-        var errors = validationResult(req).array()
-        console.log(errors)
-        if (errors.length) {
-            res.json({errors: errors})
-        }else {
-            productController.createProduct(req, res)
-        }
-})
-
-router.post('/create/worker', [
-    check('username').notEmpty().withMessage('Username is required'),
-    check('email').notEmpty().withMessage('Email is required'),
-    check('email').isEmail().withMessage('Email is not valid'),
-    check('pass').notEmpty().withMessage('Password is required').custom((value, {req}) => {
-        if(value !== req.body.repass){
-            throw new Error("Passwords do not match");
-        }
-        return true;
-    })
-],  function(req, res){
-        var errors = validationResult(req).array()
-        console.log(errors)
-        if (errors.length) {
-            res.json({errors: errors})
-        }else {
-            workerController.createWorker(req, res)
-        }
-})
+    }
+}
 
 // remove this function altogether
 function handleErrors(req, res, callback, errorPage){
