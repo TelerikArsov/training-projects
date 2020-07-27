439891c078fa8d3497e7d80c9bbda4c8bddfe2e1
TelerikArsov <telerik007@gmail.com>
2020-07-27 12:06:50 +0300

    Changed the way the functions in the controller behave.

--- shop/controllers/tag_category_controller.js
+++ shop/controllers/tag_category_controller.js
@@ -1,90 +1,92 @@
 var db = require('./db');
 
-function create(table, args, callback, cb_args){
+function create(table, args, callback){
     db.query('INSERT INTO ' + table + ' (name, color, visible) VALUES ($1, $2, $3)',
     args, (error, results) => {
         if (error) {
             throw error;
         }
-        callback(...cb_args, error, results);
+        callback(error, results);
     });
 }
-function getAll(res, table){
+function getAll(res, table, callback){
     db.query('Select * FROM ' + table +' ;',
     (error, results) => {
         if (error) {
             throw error;
         }
-        res.json({table: table, result: results.rows});
+        callback(error, results);
+
+            //res.json({table: table, result: results.rows});
     });
 }
 
-function deleteTC(table, args, callback, cb_args){
+function deleteTC(table, args, callback){
     db.query('DELETE FROM ' + table + ' WHERE id = $1',
     args, (error, results) => {
         if (error) {
             throw error;
         }
-        callback(...cb_args, error, results);
+        callback(error, results);
     });
 }
 
-function edit(table, args, callback, cb_args){
+function edit(table, args, callback){
     db.query('UPDATE ' + table + ' SET name = $2, color = $3, visible = $4 WHERE id = $1',
     args, (error, results) => {
         if (error) {
             throw error;
         }
-        callback(...cb_args, error, results);
+        callback(error, results);
     })
 }
 
-exports.createTag = (req, res) => {
+exports.createTag = (req, res, callback) => {
     if(req.session.user && req.session.role == "admin"){
         const { name, color, visible } = req.body;
-        create('tags', [name, color, visible], getAll, [res, 'tags']);
+        create('tags', [name, color, visible], callback);
     }
 }
 
-exports.deleteTag = (req, res) => {
+exports.deleteTag = (req, res, callback) => {
     if(req.session.user && req.session.role == "admin"){
         const { id } = req.body;
-        deleteTC('tags', [id], getAll, [res, 'tags']);
+        deleteTC('tags', [id], callback);
     }
 }
 
-exports.getAllTags = (req, res) =>{
-    getAll(res, 'tags');
+exports.getAllTags = (req, res, callback) =>{
+    getAll(res, 'tags', callback);
 }
 
-exports.editTags = (req, res) => {
+exports.editTags = (req, _res, callback) => {
     if(req.session.user && req.session.role == "admin"){
         const { id, name, color, visible } = req.body;
-        edit('tags', [id, name, color, visible], getAll, [res, 'tags']);
+        edit('tags', [id, name, color, visible], callback);
     }
 }
 
-exports.createCategory = (req, res) => {
+exports.createCategory = (req, res, callback) => {
     if(req.session.user && req.session.role == "admin"){
         const { name, color, visible } = req.body;
-        create('categories', [name, color, visible], getAll, [res, 'categories']);
+        create('categories', [name, color, visible], callback);
     }
 }
 
-exports.deleteCategory = (req, res) => {
+exports.deleteCategory = (req, res, callback) => {
     if(req.session.user && req.session.role == "admin"){
         const { id } = req.body;
-        deleteTC('categories', [id], getAll, [res, 'categories']);
+        deleteTC('categories', [id], callback);
     }
 }
 
-exports.getAllCategories = (req, res) =>{
-    getAll(res, 'categories');
+exports.getAllCategories = (req, res, callback) =>{
+    getAll(res, 'categories', callback);
 }
 
-exports.editCategory = (req, res) => {
+exports.editCategory = (req, res, callback) => {
     if(req.session.user && req.session.role == "admin"){
         const { id, name, color, visible } = req.body;
-        edit('categories', [id, name, color, visible], getAll, [res, 'categories']);
+        edit('categories', [id, name, color, visible], callback);
     }
 }
