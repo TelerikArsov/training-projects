3bd6d0447ab59e1bb8b7d8a31bf3c23d62688b14
TelerikArsov <telerik007@gmail.com>
2020-08-19 16:51:06 +0300

    Base db error class.

--- shop/controllers/db.js
+++ shop/controllers/db.js
@@ -15,3 +15,19 @@ exports.query = function(text, params, callback) {
 exports.asyncQuery = async function(text, param) {
     return await pool.query(text, param); 
 };
+
+class DBerror extends Error{
+    constructor(errors, errorMsg) {
+        this.name = 'ValidateError';
+        this.error_msg = errorMsg;
+        var error = Error.call(this, this.getErrorMsg(errors.code));
+        this.message = error.message;
+        this.stack = error.stack;
+    };
+    getErrorMsg(error_code){
+        return this.error_msg[error_code] || "Unexpected error occured!"
+    }
+}
+
+
+exports.DBerror = DBerror
